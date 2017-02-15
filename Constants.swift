d5851409450073efe51af6b5da7345ff04047830
YouJCheng <jiancheng06@gmail.com>
2017-02-15 15:35:54 +0800

    Sign in UI custom complete

---
+++ Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Social
+//
+//  Created by mail on 2017/2/15.
+//  Copyright © 2017年 YuChienCheng. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY:CGFloat = 120.0/255.0
