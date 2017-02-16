de2051ba0a7e66abdccbc73d39b5a01e2edd591e
YouJCheng <jiancheng06@gmail.com>
2017-02-16 16:08:31 +0800

    Feed UI completed

---
+++ Social/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  Social
+//
+//  Created by mail on 2017/2/16.
+//  Copyright © 2017年 YuChienCheng. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likeLbl: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+  
+    
+    
+
+}
