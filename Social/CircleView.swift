0435aa235f0cc1aee2cf74eba24a2ccaffb2bd22
YouJCheng <jiancheng06@gmail.com>
2017-02-16 13:17:59 +0800

    Feed VC ui completed

--- Social/CircleView.swift
+++ Social/CircleView.swift
@@ -1,24 +1,26 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  Social
 //
-//  Created by mail on 2017/2/15.
+//  Created by mail on 2017/2/16.
 //  Copyright © 2017年 YuChienCheng. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
